b3741e00883928149f11eb46a4e15f81dd7fc0e2
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-25 18:26:52 +0200

    aggiunto ciclo for

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -13,18 +13,22 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $train = new Train();
 
-        $train->azienda = $faker->company();
-        $train->stazione_di_partenza = $faker->city();
-        $train->stazione_di_arrivo = $faker->city();
-        $train->orario_di_partenza = $faker->time();
-        $train->orario_di_arrivo = $faker->time();
-        $train->codice_treno = $faker->bothify('??-#######');
-        $train->numero_carrozze = $faker->numberBetween(1,8);
-        $train->in_orario = $faker->boolean();
-        $train->cancellato = $faker->boolean();
+        for ($i=0; $i < 20; $i++) { 
 
-        $train->save();
+            $train = new Train();
+
+            $train->azienda = $faker->company();
+            $train->stazione_di_partenza = $faker->city();
+            $train->stazione_di_arrivo = $faker->city();
+            $train->orario_di_partenza = $faker->time();
+            $train->orario_di_arrivo = $faker->time();
+            $train->codice_treno = $faker->bothify('??-#######');
+            $train->numero_carrozze = $faker->numberBetween(1,8);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+
+            $train->save();
+        }
     }
 }
